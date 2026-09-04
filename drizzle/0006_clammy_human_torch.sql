26c3108f86cc9d61c23da2b0fde6cbaa0f2dbb29
Gajraj-GeloraTech <gajraj.singh@geloratech.com>
2026-09-04 17:15:08 +0530

    fix: drizzle duplicate migration file prefix

--- drizzle/0006_clammy_human_torch.sql
+++ drizzle/0006_clammy_human_torch.sql
@@ -53,3 +53,14 @@ ALTER TABLE "bookings" DROP COLUMN "amount_paid";--> statement-breakpoint
 ALTER TABLE "bookings" DROP COLUMN "amount_received";--> statement-breakpoint
 ALTER TABLE "bookings" DROP COLUMN "return_change";--> statement-breakpoint
 ALTER TABLE "bookings" ADD CONSTRAINT "bookings_invoice_number_unique" UNIQUE("invoice_number");
+
+ALTER TABLE "attraction_management" DROP COLUMN "adult_price";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "child_price";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "student_price";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "senior_price";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "foreigner_price";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "adult_seats";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "child_seats";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "student_seats";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "senior_seats";--> statement-breakpoint
+ALTER TABLE "attraction_management" DROP COLUMN "foreigner_seats";
